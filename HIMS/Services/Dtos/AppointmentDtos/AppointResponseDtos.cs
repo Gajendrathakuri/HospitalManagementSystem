cb1face5ca079ac49307d9ec55fb15cceade7cbe
Thakuri <gaju76960@gmail.com>
2026-02-26 12:23:35 +0545

    appointment servies and dto bug fixed

--- HIMS/Services/Dtos/AppointmentDtos/AppointResponseDtos.cs
+++ HIMS/Services/Dtos/AppointmentDtos/AppointResponseDtos.cs
@@ -7,7 +7,7 @@ namespace HIMS.Services.Dtos.AppointmentDtos
             public Guid Appointmentid { get; set; }
             public DateOnly AppointmentDate { get; set; }
             public TimeOnly AppointmentTime { get; set; }
-
+                public string DoctorName { get; set; }
             public string PatientName { get; set; }
             public string PatientContact { get; set; }
             public string PatientAddress { get; set; }
